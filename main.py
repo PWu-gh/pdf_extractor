6ed8bc9f166fdc775fbcb7089077ea3d3d1b8c1a
Pwu <pwu.dev@outlook.com>
2022-03-27 16:55:30 +0200

    pdf split

---
+++ main.py
@@ -0,0 +1,134 @@
+import fitz
+import unidecode 
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTChar, LTTextBox
+from collections import Counter
+import re
+
+
+"""
+    retourne la page et le label où se trouve le sommaire
+"""
+def loc_sommaire(doc):
+    page_id = 0
+    for page in doc:
+        page_id += 1
+        txt = page.get_text()[:200].upper()
+        if "SOMMAIRE" in txt:
+            # print(page.get_label())
+            return page_id, page.get_label()
+        
+
+
+def extract_fontsize(path, page_id):
+    Extract_Data=[]
+    doc = extract_pages(path)
+    for page_layout in doc:
+        if page_layout.pageid == page_id:
+            for element in page_layout:
+                if isinstance(element, LTTextBox):
+                    for text_line in element:
+                        for character in text_line:
+                            if isinstance(character, LTChar):
+                                Font_size = int(character.size)
+                    text = unidecode.unidecode(element.get_text())
+                    text = text.replace('\n', '')
+                    Extract_Data.append([Font_size, text])
+            break
+    
+    # reconnect strings
+    corrected_data = []
+    if len(Extract_Data) > 0:
+        last_string_size = 0
+        for data in Extract_Data:
+            if last_string_size == data[0]:
+                if corrected_data[-1][1][-1:] == ' ':
+                    corrected_data[-1][1] = corrected_data[-1][1] + data[1]
+            else:
+                corrected_data.append(data)
+            last_string_size = data[0]
+
+    return corrected_data
+
+
+
+def get_titles(data):
+    tab_size = [col[0] for col in data]
+    collec_size = dict(Counter(tab_size))
+    most_common_size = max(collec_size, key=collec_size.get)
+
+
+    for i in collec_size.copy():
+        if i < most_common_size:
+            del collec_size[i]
+        elif collec_size[i] < 5:
+            del collec_size[i]
+
+    if len(collec_size) >= 2:
+        del collec_size[most_common_size]
+    title_size = max(collec_size, key=collec_size.get)
+        
+    titles = []
+    for x in data:
+        if x[0] == title_size:
+            if x[1].isdigit() == False:
+                # titles.append(x)
+                titles.append(x[1].strip())
+    return titles
+
+def pdfto_array_fitz(pdf):
+    document  = fitz.open(pdf) 
+    fitz_txt = []
+    for page in document:
+        str = unidecode.unidecode(page.get_text())
+        fitz_txt = str.split('\n')
+    return fitz_txt
+
+
+
+def gen_sommaire():
+    # titre , page
+    sommaire = []
+    for title in titles:
+        # g1 = titre
+        # g2 = annee true -> page
+        # g3 = annee false -> page
+        reg_page = r'(' + title + r')' + r' ([1-9][0-9]*) ?([0-9]*)'
+        tx = re.search(reg_page, txt)
+        # print(tx.group(3), tx.group(0))
+        if tx.group(3) == '':
+            sommaire.append([tx.group(1), int(tx.group(2))])
+        else:
+            sommaire.append([tx.group(1)+" "+tx.group(2), int(tx.group(3))])
+    return sommaire
+
+
+def gen_pdf(sommaire):
+    # file_name = './pdf_split/test.pdf'
+    for x in range(len(sommaire)):
+        from_p = sommaire[x][1]+1       # 2 premieres pages ne comptent pas
+        if x == (len(sommaire)-1):
+            to_p = document.page_count
+        else:
+            to_p = sommaire[x+1][1]
+
+        doc2 = fitz.open()
+        doc2.insert_pdf(document, from_page = from_p, to_page = to_p, start_at = 0)
+        doc2.save("./pdf_split/"+ sommaire[x][0] +".pdf")
+
+
+
+pdf = "./U_record/TF1_2020.pdf"
+# document = extract_pages(pdf)
+document  = fitz.open(pdf) 
+
+som_page = loc_sommaire(document)[0]
+tab_fontsize = extract_fontsize(pdf, som_page)
+titles = get_titles(tab_fontsize)
+
+
+txt = document[som_page-1].get_text()
+txt = unidecode.unidecode(txt)
+txt = txt.replace('\n', ' ').replace('  ', ' ')
+sommaire = gen_sommaire()
+gen_pdf(sommaire)
